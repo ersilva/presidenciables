645f27b5bb35dc27208363854e38888194585f7f
Erika <erika@Erika-Ubuntu.(none)>
2013-05-30 17:27:17 -0400

    agregado related_name a las fk

--- PP/Promesas/models.py
+++ PP/Promesas/models.py
@@ -6,12 +6,12 @@ class Partido(models.Model):
 	info = models.CharField(max_length=100)
 
 class Candidato(models.Model):
-	partido = models.ForeignKey(Partido)
+	partido = models.ForeignKey(Partido, related_name = "candidatos")
 	nombre = models.CharField(max_length=100)
 	info = models.CharField(max_length=100)
 	
 class Promesa(models.Model):
-	candidato = models.ForeignKey(Candidato)
+	candidato = models.ForeignKey(Candidato, related_name = "promesas")
 	informante = models.CharField(max_length=100)
 	fuente = models.CharField(max_length=200)
 	fecha = models.DateTimeField('date published')
